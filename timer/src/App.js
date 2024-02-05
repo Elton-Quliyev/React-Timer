50dda5bf16f5fbf2a1d3daa9346dc6c73cc4792c
Elton-Quliyev <equliyev118@gmail.com>
2024-02-05 21:44:25 +0400

    .

---
+++ timer/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import TimeCounter from './components/Time';
+
+function App() {
+  return (
+    <>
+    
+    <TimeCounter deqiqe={0}
+      saniye={5} />
+    
+    
+    </>
+    
+    
+  );
+}
+
+export default App;
---
+++ timer/src/components/Time/index.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+const TimeCounter = ({ deqiqe, saniye }) => {
+  const [second, setSecond] = useState(saniye);
+  const [minute, setMinute] = useState(deqiqe);
+  const [isTimerRunning, setIsTimerRunning] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (second === 0 && minute === 0) {
+        clearInterval(interval);
+        setIsTimerRunning(false);
+      } else {
+        setSecond((second) => (second === 0 ? 59 : second - 1));
+        setMinute((prevMinute) =>
+          second === 0 ? (minute > 0 ? minute - 1 : 0) : minute
+        );
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [second, minute]);
+
+  const formattedSecond = second < 10 ? `0${second}` : `${second}`;
+  const formattedMinute = minute < 10 ? `0${minute}` : `${minute}`;
+
+
+
+  const handleRestart = () => {
+    setSecond(saniye);
+    setMinute(deqiqe);
+    setIsTimerRunning(true);
+  };
+  return (
+    <div>
+      <h1>zaman: {formattedMinute} : {formattedSecond}</h1>
+      {!isTimerRunning && <button onClick={handleRestart}>Yenidən Başla</button>}
+    </div>
+  );
+};
+
+export default TimeCounter;
